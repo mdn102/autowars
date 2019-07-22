c7fb95d803e66d0a56a0a9e987b75e0f0f72d023
Kat Grennan <caitlyn.grennan@techtonic.com>
2019-07-22 15:39:53 -0600

    Clean up redundant code

--- src/com/boulder/autowars/Utils.java
+++ src/com/boulder/autowars/Utils.java
@@ -3,13 +3,7 @@ package com.boulder.autowars;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.util.Date;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
@@ -30,10 +24,7 @@ public class Utils {
     public static class DateDiff {
 
         public static int inDays(LocalDate d1, LocalDate d2) {
-
-            int diff = (int) DAYS.between(d1, d2);
-            System.out.println(diff);
-            return diff;
+            return (int) DAYS.between(d1, d2);
         }
 
     }
